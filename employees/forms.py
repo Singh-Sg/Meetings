41febc182dcb6be008f3f4f935c36257542ef7bb
Upendra <upendrasinghit@gmail.com>
2021-06-29 10:24:51 +0530

    # Deleting unused files

--- employees/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from employees.models import EmployeeSlots
-
-
-class EmployeeSlotsForm(forms.ModelForm):
-    class Meta:
-        model = EmployeeSlots
-        fields = "__all__"
